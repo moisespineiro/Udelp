37a5473a34dd41e588f319d064023b09d8943341
moisespineiro <angelestradamx@hotmail.com>
2020-03-17 22:40:40 -0600

    Encuestas,moy

--- Encuestas/app/src/main/java/com/example/encuestas/Util/ValidateEmail.kt
+++
@@ -1,9 +0,0 @@
-package com.example.encuestas.Util
-
-class ValidateEmail {
-    companion object{
-        fun isEmailValid(email: String): Boolean {
-            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-        }
-    }
-}
